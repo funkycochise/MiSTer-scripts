e62de2053423f9b4365a83c9e53bab145030c545
Naku <66411791+nakuakaben@users.noreply.github.com>
2020-06-07 22:05:15 +0200

    Update 1.2 to 1.2.1
    change var

--- updater_clm.sh
+++ updater_clm.sh
@@ -13,7 +13,14 @@ SCRIPT_URL="$GIT/nakuakaben/MiSTer-scripts/raw/master/$SCRIPT_SH"
 MISTER_PATH="$HOME/MiSTer/"
 GET_PNG="$GIT/nakuakaben/MiSTer-scripts/raw/master/MiSTer/"
 
-VERSION="1.1"
+VERSION="1.2.1"
+
+# logo
+OPTION1="color.png"
+OPTION2="japan.png"
+OPTION3="nologo.png"
+OPTION4="mylogo.png"
+OPTION5="logo.png"
 
 # code start here
 clear
@@ -21,7 +28,7 @@ clear
 # check for the necessary files
 echo -e "Check folder/files and Update $VERSION \n" # display updater version
 
-options=("color.png" "japan.png" "nologo.png" "mylogo.png")
+options=("$OPTION1" "$OPTION2" "$OPTION3" "$OPTION4" "$OPTION5")
 for p in "${options[@]}" # pour $p choissir l'element [@] dans la variable "options" 
 do
 echo -e "MiSTer/$p ... \c"
