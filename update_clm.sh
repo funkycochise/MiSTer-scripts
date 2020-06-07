285546adc0336edb3321a61a25f7438578d3f48b
Naku <66411791+nakuakaben@users.noreply.github.com>
2020-06-08 00:36:25 +0200

    Update and rename update.sh to update_clm.sh

--- update_clm.sh
+++ update_clm.sh
@@ -11,8 +11,6 @@ clear
 echo "Downloading..."
 	wget -q -t 3 --output-file=/tmp/wget-log --show-progress -O /tmp/$SCRIPT_SH $SCRIPT_URL # downloading to output /tmp
 	chmod +x /tmp/$SCRIPT_SH # make executable script
-
-echo -en "Starting $SCRIPT_SH" ; sleep 2
 	/tmp/$SCRIPT_SH # run script 
 	rm -f /tmp/$SCRIPT_SH # clean file in /tmp at the end
 
