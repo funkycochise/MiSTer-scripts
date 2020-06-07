0d8a1d9a839385a91e17e78590b5a6a284e464b0
Naku <66411791+nakuakaben@users.noreply.github.com>
2020-06-08 01:53:02 +0200

    Version 1.0

--- updater_clm.sh
+++ updater_clm.sh
@@ -12,6 +12,6 @@ echo "Downloading..."
 	wget -q -t 3 --output-file=/tmp/wget-log --show-progress -O /tmp/$SCRIPT_SH $SCRIPT_URL # downloading to output /tmp
 	chmod +x /tmp/$SCRIPT_SH # make executable script
 	/tmp/$SCRIPT_SH # run script 
-	rm -f /tmp/$SCRIPT_SH # clean file in /tmp at the end
+	rm -f /tmp/$SCRIPT_SH # clean file in /tmp at the end 
 
 exit 0
