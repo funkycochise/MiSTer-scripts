fa253147df15be999aa7856358ebd7f675f93060
Naku <66411791+nakuakaben@users.noreply.github.com>
2020-06-07 22:10:59 +0200

    Add files via upload

---
+++ releases/CLM_200607/clm_script.sh
@@ -0,0 +1,193 @@
+#!/bin/bash
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Copyright 2020-2021 "nakuakaben"
+
+# You can download the latest version of this script from:
+# https://github.com/nakuakaben/MiSTer-scripts
+
+# Version 1.1   - 2020-06-04 - new layout and some minor fixes
+# Version 1.0   - 2020-06-03 - First commit
+
+BASE_PATH="$HOME"
+MISTER_PATH="${BASE_PATH}/MiSTer/"
+
+# logo
+OPTION1="color.png"
+OPTION2="japan.png"
+OPTION3="nologo.png"
+OPTION4="mylogo.png"
+
+
+# ========= OPTIONS ==================
+URL="https://github.com"
+SCRIPT_URL="${URL}/nakuakaben/MiSTer-scripts/blob/master/clm_script.sh"
+MISTER_URL="${URL}/MiSTer-devel/"
+CLM_VERSION="1.1"
+
+# code start here
+echo -e "\n Change logo of mister $CLM_VERSION"
+
+echo -e "\n\033[1;33m Do you want to delete the Main_MiSter directory and/or the MiSTer file ? \033[0m \n"
+
+function yesfile {
+        echo -e "\033[1;31m Delete old MiSTer file... \033[0m \c" 
+            rm -f "${MISTER_PATH}MiSTer"
+        echo -e "done \n"
+}
+function both {
+        echo -e "\033[1;31m Delete old folder Main_MiSter and MiSTer file... \033[0m \c" 
+            rm -rf "${MISTER_PATH}Main_MiSter/"
+            rm -f "${MISTER_PATH}MiSTer"
+        echo -e "done \n"
+}
+
+PS3='Please enter your choice: '
+options=("yes (file)" "both")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "yes (file)")
+            echo -e "\033[1;32m You choice is\033[0m \033[1;37m....$opt\033[0m"
+            yesfile 
+            break
+            ;;
+        "both")
+            echo -e "\033[1;32m You choice is\033[0m \033[1;37m....$opt\033[0m"
+            both
+            break
+            ;;
+        *) echo -e "\033[1;31m Lost ?\033[0m, please select: \n 1) yes \n 2) no \n 3) next";;
+        
+    esac
+done
+ 
+#Git
+
+echo -e "\n\033[1;33m Do you want to get the source from Github ? \033[0m \n https://github.com/MiSTer-devel/ \n"
+
+function clone {
+        cd "$MISTER_PATH"
+        git clone "${MISTER_URL}Main_MiSter.git"
+}
+function update {
+        cd "$MISTER_PATH"
+        git pull "${MISTER_URL}Main_MiSter.git"
+}
+
+PS3='Please enter your choice: '
+options=("Clone" "Update" "Next")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Clone")
+            clone 
+            break
+            ;;
+        "Update")
+            echo -e "\033[1;31m Not yet implemented.\033[0m\n" 
+            ;;
+        "Next")
+            break
+            ;;
+        *) echo -e "\033[1;31m Lost ?\033[0m, please select: \n 1) Clone \n 2) Update \n 3) Next step";;
+    esac
+done
+ 
+#logo
+
+echo -e "\n\033[1;33m Do you want to change the logo displayed on the screen ? \033[0m \n"
+function color {
+        echo -e "\033[1;32m The\033[0m \033[1;37m$opt\033[0m \033[1;32mfile will replace\033[0m \033[1;37mlogo.png\033[0m"
+        cp -v "${MISTER_PATH}$opt" "${MISTER_PATH}Main_MiSter/logo.png"  
+        echo ""  
+}
+function japan {
+        echo -e "\033[1;32m The\033[0m \033[1;37m$opt\033[0m \033[1;32mfile will replace\033[0m \033[1;37mlogo.png\033[0m"
+        cp -v "${MISTER_PATH}$opt" "${MISTER_PATH}Main_MiSter/logo.png"   
+        echo "" 
+}
+function nologo {
+        echo -e "\033[1;32m The\033[0m \033[1;37m$opt\033[0m \033[1;32mfile will replace\033[0m \033[1;37mlogo.png\033[0m"
+        cp -v "${MISTER_PATH}$opt" "${MISTER_PATH}Main_MiSter/logo.png"     
+        echo ""
+}
+function mylogo {
+        echo -e "\033[1;32m The\033[0m \033[1;37m$opt\033[0m \033[1;32mfile will replace\033[0m \033[1;37mlogo.png\033[0m"
+        cp -v "${MISTER_PATH}$opt" "${MISTER_PATH}Main_MiSter/logo.png"     
+        echo ""
+}
+
+PS3='Please enter your choice: '
+options=("$OPTION1" "$OPTION2" "$OPTION3" "$OPTION4"  "Keep")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "$OPTION1")
+            color 
+            break
+            ;;
+        "$OPTION2")
+            japan
+            break
+            ;;
+        "$OPTION3")
+            nologo
+            break
+            ;;
+        "$OPTION4")
+            mylogo
+            break
+            ;;
+        "Keep")
+            break
+            ;;
+        *) echo -e "\033[1;31m Lost ?\033[0m, please select: \n 1) $OPTION1 \n 2) $OPTION2 \n 3) $OPTION3 \n 4) $OPTION4 \n 5) keep \n";;
+    esac
+done
+
+echo -en "\n\033[1;33m Preparing the directory for compilation\033[0m"; sleep 0.05; echo -n "."; sleep 0.05; echo -n "."; sleep 0.05; echo -n "."; sleep 0.05; echo -n "."; sleep 0.05; sleep 0.05; echo -n "."; sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n "."; echo -n "]";
+echo ""
+function yes {
+        echo -e "\n\033[1;32m Compilation of the MiSTer file in progress...\033[0m\n Estimated waiting time is about 1 minute.\n"
+        cd "${MISTER_PATH}Main_MiSter/" && make -i | pv -tr >make.tmp
+}
+
+PS3='Please enter your choice: '
+options=("Yes" "Skip" )
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Yes")
+            yes 
+            break
+            ;;
+        "Skip")
+            echo "you chose choice $REPLY which is $opt"
+            break
+            ;;
+        *) echo -e "\033[1;31m Lost ?\033[0m, please select: \n 1) silent \n 2) verbose \n 3) no \n 4) next";;
+    esac
+done
+echo "" 
+    echo -e "\033[1;32mCopy MiSTer file on ${MISTER_PATH} \033[0m"
+        cp -v "${MISTER_PATH}Main_MiSter/MiSTer" ${MISTER_PATH}  
+echo ""
+
+echo -e "\033[1;37m                          -\_(O_o)_/-                        \033[0m"
+echo -e "\033[1;36m-------------------------------------------------------------\033[0m"
+echo -e "\033[1;37m     You can copy file ${MISTER_PATH}MiSTer on your FPGA     \033[0m"
+echo -e "\033[1;36m-------------------------------------------------------------\033[0m\n"
+exit 0
---
+++ releases/CLM_200607/updater_clm.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# You can download the latest version of this script from:
+# https://github.com/nakuakaben/MiSTer-scripts
+# Version 1.2 - 2020-06-07 - Adding the content verification needed for CLM
+# Version 1.1 - 2020-06-07 - Modification of the code: use of temporary directory and cleaning at the end of the script.
+# Version 1.0 - 2020-06-04 - First commit.
+
+# variable
+GIT="https://github.com"
+SCRIPT_SH="clm_script.sh"
+SCRIPT_URL="$GIT/nakuakaben/MiSTer-scripts/raw/master/$SCRIPT_SH"
+MISTER_PATH="$HOME/MiSTer/"
+GET_PNG="$GIT/nakuakaben/MiSTer-scripts/raw/master/MiSTer/"
+
+VERSION="1.2.1"
+
+# logo
+OPTION1="color.png"
+OPTION2="japan.png"
+OPTION3="nologo.png"
+OPTION4="mylogo.png"
+OPTION5="logo.png"
+
+# code start here
+clear
+
+# check for the necessary files
+echo -e "Check folder/files and Update $VERSION \n" # display updater version
+
+options=("$OPTION1" "$OPTION2" "$OPTION3" "$OPTION4" "$OPTION5")
+for p in "${options[@]}" # pour $p choissir l'element [@] dans la variable "options" 
+do
+echo -e "MiSTer/$p ... \c"
+    if [ ! -f "$MISTER_PATH${p}" ]; then
+        if curl --location --head --fail -k --silent "$GET_PNG${p}" >/dev/null; then
+            echo "downloading"
+            wget "$GET_PNG${p}" -t 3 --show-progress -q --no-check-certificate -nc -c -P $MISTER_PATH
+        else
+	    echo "not found"
+        fi
+    else
+	echo "done"
+    fi
+done
+
+echo -e "\nDownloading last version of CLM"
+	wget -q -t 3 --output-file=/tmp/wget-log --show-progress -O /tmp/$SCRIPT_SH $SCRIPT_URL # downloading to output /tmp
+	chmod +x /tmp/$SCRIPT_SH # make executable script
+	/tmp/$SCRIPT_SH # run script 
+	rm -f /tmp/$SCRIPT_SH # clean file in /tmp at the end
+exit 0
