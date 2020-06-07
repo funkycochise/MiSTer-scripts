b17c03c9f5d3aaa15530d5b4a6f37a2e499eeb58
Naku <66411791+nakuakaben@users.noreply.github.com>
2020-06-07 14:08:26 +0200

    Update updater_clm_v1.1.sh

--- beta/updater_clm_v1.1.sh
+++ beta/updater_clm_v1.1.sh
@@ -1,61 +1,27 @@
 #!/bin/bash
 
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-# Copyright 2020-2021 Aka "Naku" Ben
-
 # You can download the latest version of this script from:
 # https://github.com/nakuakaben/MiSTer-scripts
-
 
 # Version 1.1 - 2020-06-07 - Modification of the code: use of temporary directory and cleaning at the end of the script.
 # Version 1.0 - 2020-06-04 - First commit.
 
-#=========   USER OPTIONS   =========
-#Url
+#v ariable
 GIT="https://github.com"
 SCRIPT_SH="clm_script.sh"
 SCRIPT_URL="$GIT/nakuakaben/MiSTer-scripts/raw/master/$SCRIPT_SH"
-
-#========= ADVANCED OPTIONS =========
-
-
-#========= CODE STARTS HERE =========
-clear
-
-CLM="CLM_script"
 UPDATER_VERSION="1.1"
 
-echo -e "\033[1;36m +---------------------------+\033[0m"
-echo -e "\033[1;36m |  Check for Updater version ${UPDATER_VERSION}  |\033[0m"
-echo -e "\033[1;36m +---------------------------+\033[0m"
+# code start here
+clear
+echo "Updater $UPDATER_VERSION" # display updater version
 
-echo -e "\nDownloading last version of $CLM \n"
-    wget -q -t 3 --output-file=/tmp/wget-log --show-progress -O /tmp/$SCRIPT_SH $SCRIPT_URL
-	# curl -O "${SCRIPT_URL}"
-echo ""
-echo -e "Make executable $CLM...\c"
-	chmod +x /tmp/$SCRIPT_SH
+echo "Downloading..."
+	wget -q -t 3 --output-file=/tmp/wget-log --show-progress -O /tmp/$SCRIPT_SH $SCRIPT_URL # downloading to output /tmp
+	chmod +x /tmp/$SCRIPT_SH # make executable script
 echo "done"
 
-echo -en "\n\033[1;33m Run script \033[0m"; sleep 0.05; echo -n "."; sleep 0.05; echo -n "."; sleep 0.05; echo -n "."; sleep 0.05; echo -n "."; sleep 0.05; sleep 0.05; echo -n "."; sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n "."; echo -n "]";
-echo ""
-	/tmp/$SCRIPT_SH
-
-echo -e "Cleaning.........\c"	
-    rm -f /tmp/$SCRIPT_SH
-echo "done"
-echo ""
-
+echo -en "Run script"; sleep 0.05; echo -n "."; sleep 0.05; echo -n "."; sleep 0.05; echo -n "."; sleep 0.05; echo -n "."; sleep 0.05; sleep 0.05; echo -n "."; sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n ".";sleep 0.05; echo -n "."; echo -n "]";
+	/tmp/$SCRIPT_SH # run script 
+	rm -f /tmp/$SCRIPT_SH # clean file in /tmp at the end
 exit 0
