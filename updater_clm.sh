e2a8c78551288559d2c079ff372532100fa530e9
Naku <66411791+nakuakaben@users.noreply.github.com>
2020-06-08 04:02:45 +0200

    Add: option for LXDE in MiSTer FPGA

--- updater_clm.sh
+++ updater_clm.sh
@@ -16,13 +16,15 @@ echo "Downloading..."
 	
 # if you use this script on FPGA - LXDE
 
-echo -e "Update file on FPGA... \c"
+echo -e " ---------------------- \c"
+echo -e " Update file on FPGA... \c"
 if [ "$(uname -n)" != "MiSTer" ]; then
 		echo "not possible"
 			echo -e "\n This option is only available under LXDE on the MiSTer FPGA."
-			echo -e " Copy MiSTer (file) on your SD Card /media/fat/ or upload \n"
+			echo -e " Copy MiSTer (file) on your SD Card /media/fat/ or upload. \n" 
 		exit 1
         else 
+	echo "done"
 	cp -v "$HOME/MiSTer/MiSTer" /media/fat/
 fi
 
