cef441c70aab4b3838c16d4c3eda3b0737ced0f1
Naku <66411791+nakuakaben@users.noreply.github.com>
2020-06-08 03:30:10 +0200

    Add line for FPGA on LXDE

--- updater_clm.sh
+++ updater_clm.sh
@@ -13,5 +13,16 @@ echo "Downloading..."
 	chmod +x /tmp/$SCRIPT_SH # make executable script
 	/tmp/$SCRIPT_SH # run script 
 	rm -f /tmp/$SCRIPT_SH # clean file in /tmp at the end 
+	
+# if you use this script on FPGA - LXDE
+
+if [ "$(uname -n)" != "MiSTer" ]; then
+		echo "This option is only"
+		echo "on a MiSTer system."
+		exit 1
+        else 
+	echo -en "Update file on FPGA ...";cp -v "$HOME/MiSTer/MiSTer" /media/fat/
+
+fi
 
 exit 0
